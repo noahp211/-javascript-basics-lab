973fbc42126be6528c4c3bd913a55fb03ddd9927
noahp211 <168378818+noahp211@users.noreply.github.com>
2025-01-25 00:43:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let greeting = "Hello, World!";
+console.log(greeting);
+
+let studentName = "Noah Park";
+let studentAge = 24;
+let isEnrolled = true;
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Is Enrolled:", isEnrolled);
+
+let exampleString = "learning java";
+console.log("String Example:", exampleString);
+
+
+let exampleNumber = 21;
+console.log("Number Example:", exampleNumber);
+
+let exampleBoolean = false;
+console.log("Boolean Example:", exampleBoolean);
+
+
+let exampleArray = ["apple", "banana", "cherry"];
+console.log("Array Example:", exampleArray);
+
+// Object
+let exampleObject = { firstName: "John", lastName: "Doe" };
+console.log("Object Example:", exampleObject);
+
+
+console.log("Addition (10 + 5):", 10 + 5);
+console.log("Subtraction (10 - 5):", 10 - 5);
+console.log("Multiplication (10 * 5):", 10 * 5);
+console.log("Division (10 / 5):", 10 / 5);
+
+
+console.log("10 > 5:", 10 > 5);
+console.log("10 < 5:", 10 < 5);
+console.log("10 === 10:", 10 === 10);
+console.log("10 !== 5:", 10 !== 5);
+
+console.log("true && false:", true && false);
+console.log("true || false:", true || false);
+console.log("!true:", !true);
